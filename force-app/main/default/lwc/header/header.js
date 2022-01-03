313dc6577cd5714f5be2d0ce84cf46a081c3ac3e
Jae Cho <jcho7@pride.hofstra.edu>
2022-01-03 18:53:56 -0500

    more lwc components

---
+++ force-app/main/default/lwc/header/header.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import keeblogo from '@salesforce/resourceUrl/keeblogo1';
+
+export default class Header extends LightningElement {
+    keebUrl = keeblogo;
+}
